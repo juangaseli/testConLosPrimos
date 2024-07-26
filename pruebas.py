918ad1d21d485cc6eb3ce255b4bb64b50b316643
Terrazzino <terrazzinofranco1996@gmail.com>
2024-07-26 19:02:48 -0300

    Modificacion a variable extencion en pruebas .py

--- pruebas.py
+++ pruebas.py
@@ -1,4 +1,5 @@
 initial_extensions = [
   'cogs.user_commands', 'cogs.copypaste_commands', 'cogs.nsfw_commands',
-  'cogs.price_commands', 'puto_el_que_lee', 'Lesbiana la que lee'
+  'cogs.price_commands', 'puto_el_que_lee', 'Lesbiana la que lee',
+  'Leo se ela come'
 ]
